2d3000e9b29f6a19a4d5c9f234cc762fcef44e58
Manish Kumar <manishleoy2k3@gmail.com>
2021-08-18 20:21:41 +0530

    Create HackerRank_InString.java
    matching the given string is subset of hackerrank or not

---
+++ src/hackerrank/strngs/HackerRank_InString.java
@@ -0,0 +1,31 @@
+package hackerrank.strngs;
+
+public class HackerRank_InString {
+
+	/*
+     * Complete the 'hackerrankInString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String hackerrankInString(String s) {
+    	String hack = "hackerrank";
+    	int j = 0;
+    	
+    	if (s.length() < hack.length()) return "NO";
+                
+        for (int i = 0; i < s.length(); i++) {
+            if (j < hack.length() && s.charAt(i) == hack.charAt(j)) {
+            	j++;
+            }
+        }
+        if(j == hack.length())
+        	return "YES";
+        else return "NO";
+    }
+    
+	public static void main(String[] args) {
+		System.out.println(HackerRank_InString.hackerrankInString("rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktjjt"));
+	}
+}

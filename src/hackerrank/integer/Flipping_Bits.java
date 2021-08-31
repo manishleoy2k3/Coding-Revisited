eb6a05784b07fea846ebb533717b6242aef5b6a2
Manish Kumar <manishleoy2k3@gmail.com>
2021-08-31 09:54:55 +0530

    Create Flipping_Bits.java
    converting decimal to binary and then flipping the bits and returning the corresponding decimal

---
+++ src/hackerrank/integer/Flipping_Bits.java
@@ -0,0 +1,44 @@
+package hackerrank.integer;
+
+public class Flipping_Bits {
+	static long pow(int base , int exponent)
+    {
+        long result = 1;
+        while( exponent != 0)
+        {
+            result *= base;
+            --exponent;
+        }
+        return result;
+    }
+	
+	public static long flippingBits(long n){
+		int[] arr = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		long k=0; int j=arr.length-1;		
+		//converting decimal to binary
+		while(n>0) {
+			k = n%2;
+			n = n/2;
+			arr[j] = (int) k;
+			j--;
+		}
+		//flip the bits
+		for(int i=0; i < arr.length; i++) {
+			if(arr[i] ==1)
+				arr[i] = 0;
+			else
+				arr[i] = 1;
+		}
+		//converting binary back to decimal
+		long decimal = 0; int exp = 0;
+        for( int i = arr.length - 1; i > -1; i--){
+            decimal += arr[i] * pow(2, exp);
+            exp++;
+        }
+        return decimal;
+	}
+	public static void main(String[] args) {
+		long n=35601423;
+		System.out.println(Flipping_Bits.flippingBits(n));
+	}
+}

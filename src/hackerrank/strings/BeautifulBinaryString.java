d027ea9ac435e5234961e4da5e906ee0cf169d0f
Manish Kumar <manishleoy2k3@gmail.com>
2021-09-14 15:20:02 +0530

    Update BeautifulBinaryString.java

--- src/hackerrank/strings/BeautifulBinaryString.java
+++ src/hackerrank/strings/BeautifulBinaryString.java
@@ -1,11 +1,20 @@
 package hackerrank.strings;
 
 public class BeautifulBinaryString {
+    /*
+     * Complete the 'beautifulBinaryString' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING b as parameter.
+     */
 
-	public static void main(String[] args) {
-		
-		String str = "0100101010";
-
-	}
-
+    public static int beautifulBinaryString(String b) {
+    	int len =b.length(), newLength = b.replaceAll("010", "").length();
+    	
+    	return (len - newLength)/3;
+    }
+    
+    public static void main(String[] args) {
+	System.out.println(BeautifulBinaryString.beautifulBinaryString("0100101010"));
+    }
 }

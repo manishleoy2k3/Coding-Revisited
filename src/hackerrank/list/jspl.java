27f2cfe744d5836b151719738f1e140fcdd67b14
Manish Kumar <manishleoy2k3@gmail.com>
2021-12-01 09:59:29 +0530

    Create jspl.java

---
+++ src/hackerrank/list/jspl.java
@@ -0,0 +1,36 @@
+S No.	DATE	PERSON NAME	AMOUNT	REMARK	
+1	22/3/2021	CASIFY	45600	For 1 ONE PLUS NORD & 2 POCO F1 MOBILE	
+2	24/3/2021	AKHILESH SBI	25000	For 1 ONE PLUS NORD MOBILE	
+3	24/3/2021	CASIFY	57000	For 3 ONE PLUS PLUS 6T	
+4	27/3/2021	CASIFY	40000	For 4 POCO F1 MOBILE	
+5	27/3/2021	CASIFY	25000	For 1 ONE PLUS NORD MOBILE	
+6	4-Feb	AKHILESH SBI	58000	For 2 ONE PLUS 6T & 2 POCO F1 MOBILE	
+7	4-May	AKHILESH SBI	66000	For Safety belt	
+8	4-May	AKHILESH SBI	6000	For Tools	
+9	21/4/2021	AKHILESH SBI	50000	To Akhilesh For Bank transfer to JSPL account	
+10	5-Nov	AKHILESH SBI	20000	To Akhilesh For employees daily expenses	
+11	25/5/2021	JSPL HDFC	80000	To JSPL for First investment amount	
+12	26/5/2021	JSPL HDFC	170000	To JSPL for employees salary	
+13	6-Jul	JSPL HDFC	98000	For Kits of 2 teams in Ranchi	
+14	5-Jul	Paytm to Akhilesh	5500	For Ticket	
+15	14-Jul	JSPL HDFC	100000	For Red Mango Team expense	
+16	20-Jul	JSPL HDFC	200000	For June month staff salary	
+17	26-Jul	JSPL HDFC	125000	teams laptop	
+18	9-Aug	DTDC UPI	7000	For Mobile Courier by DTDC	
+19	14-Aug	JSPL HDFC	30000	For Team daily expense	
+20	18-Aug	JSPL HDFC	65000	For JSPL Guest house rent	
+21	7-Sep	JSPL HDFC	30000	Sep Team Expense	
+22	9-Sep	JSPL HDFC	50000	Sep Team Expense	
+23	13-Sep	JSPL HDFC	100000	Sep Team Expense	 
+24	19-Sep	JSPL HDFC	400000	TI Team Aug salary	
+25	22-Sep	JSPL HDFC	200000	RF Team Aug salary	
+26	26-Sep	Samir Jha	112000	Samir Jha first installment	
+27	18-Oct	JSPL HDFC	100000	Teams Expense Oct 21	
+28	19-Oct	JSPL HDFC	100000	Other Expenses Oct 2021	
+29	20-Oct	JSPL HDFC	100000	Other Expenses Oct 2021	
+30	22-Nov	JSPL HDFC	50000	Teams Expense Oct 21	
+31	23-Nov	JSPL HDFC	100000	Team Salary Oct 2021	
+32	25-Nov	JSPL HDFC	100000	Team Salary Oct 2021	
+33	27-Nov	JSPL HDFC	150000	Team Salary Oct 2021	
+34	30-Nov	JSPL HDFC	200000	Team Salary Oct 2021	
+Total			3065100		
